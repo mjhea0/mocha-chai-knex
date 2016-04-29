b2d7ee343d028ec110ac1bea252976e79bae617c
Michael Herman <hermanmu@gmail.com>
2016-04-29 07:06:42 -0600

    updated seeds

--- db/seeds/development/shows_seed.js
+++ db/seeds/development/shows_seed.js
@@ -1,41 +1,36 @@
 exports.seed = function(knex, Promise) {
-  return Promise.join(
-    // Deletes ALL existing entries
-    knex('shows').del(),
-    // Inserts seed entries
-    knex('shows').insert({
-      name: 'Suits',
-      channel: 'USA Network',
-      genre: 'Drama',
-      rating: 3,
-      explicit: false
-    })
-    .then(function(response) {
+  return knex('shows').del() // Deletes ALL existing entries
+    .then(function() { // Inserts seed entries one by one in series
+      return knex('shows').insert({
+        name: 'Suits',
+        channel: 'USA Network',
+        genre: 'Drama',
+        rating: 3,
+        explicit: false
+      });
+    }).then(function () {
       return knex('shows').insert({
         name: 'Game of Thrones',
         channel: 'HBO',
         genre: 'Fantasy',
         rating: 5,
         explicit: true
-      })
-      .then(function(response) {
-        return knex('shows').insert({
-          name: 'South Park',
-          channel: 'Comedy Central',
-          genre: 'Comedy',
-          rating: 4,
-          explicit: true
-        })
-        .then(function(response) {
-          return knex('shows').insert({
-            name: 'Mad Men',
-            channel: 'AMC',
-            genre: 'Drama',
-            rating: 3,
-            explicit: false
-          });
-        });
       });
-    })
-  );
+    }).then(function () {
+      return knex('shows').insert({
+        name: 'South Park',
+        channel: 'Comedy Central',
+        genre: 'Comedy',
+        rating: 4,
+        explicit: true
+      });
+    }).then(function () {
+      return knex('shows').insert({
+        name: 'Mad Men',
+        channel: 'AMC',
+        genre: 'Drama',
+        rating: 3,
+        explicit: false
+      });
+    });
 };
